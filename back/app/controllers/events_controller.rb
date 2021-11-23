fd0922be26fd485ebddb1e40149f3724723b5a0e
Taka2401 <tkht2401@gmail.com>
2021-11-23 18:59:02 +0900

    イベント詳細取得API

--- back/app/controllers/events_controller.rb
+++ back/app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 class EventsController < ApplicationController
+
   def index
     # イベントデータの一覧を返す
     render json: Event.all
+  end
+
+  def show
+    # 指定したidのイベントデータを返す
+    render json: Event.find(params[:id])
   end
 end
