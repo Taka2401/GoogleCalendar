9e3ca133e08d016d8426445a90043e63edf6e466
Taka2401 <tkht2401@gmail.com>
2021-11-25 19:17:20 +0900

    イベント作成API

--- back/app/controllers/events_controller.rb
+++ back/app/controllers/events_controller.rb
@@ -9,4 +9,20 @@ class EventsController < ApplicationController
     # 指定したidのイベントデータを返す
     render json: Event.find(params[:id])
   end
+
+  def create
+    # パラメータの値でイベントデータを作成する
+    event = Event.new(event_params)
+    if event.save
+      render json: event
+    else
+      render json: event.errors, status: 422
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:id, :name, :start, :end, :timed, :description, :color)
+  end
 end
