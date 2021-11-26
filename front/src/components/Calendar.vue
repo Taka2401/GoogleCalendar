2a4017b3b35ef14769b6407680c386c7727145ca
Taka2401 <tkht2401@gmail.com>
2021-11-26 11:24:35 +0900

    カレンダーに予定を表示

--- front/src/components/Calendar.vue
+++ front/src/components/Calendar.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <v-sheet height="100vh">
-      <v-calendar></v-calendar>
+    <v-sheet height="6vh" class="d-flex align-center">
+      <v-toolbar-title>2020年 10月</v-toolbar-title>
+    </v-sheet>
+    <v-sheet height="94vh">
+      <v-calendar
+        v-model="value"
+        :events="events"
+        @change="fetchEvents"
+      ></v-calendar>
     </v-sheet>
   </div>
 </template>
@@ -10,6 +17,11 @@
 import { mapGetters, mapActions } from "vuex";
 
 export default {
+  data() {
+    return {
+      value: new Date('2021/11/26')
+    }
+  },
   computed: {
     ...mapGetters("events", ["events"]),
   },
