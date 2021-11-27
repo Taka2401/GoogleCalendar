737e19030770c47b05fb4cf71982bedcc0025061
Taka2401 <tkht2401@gmail.com>
2021-11-27 14:56:36 +0900

    add: 予定作成ダイアログの作成

--- front/src/components/Calendar.vue
+++ front/src/components/Calendar.vue
@@ -22,11 +22,13 @@
           (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
         "
         @click:event="showEvent"
+        @click:day="initEvent"
       ></v-calendar>
     </v-sheet>
 
     <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
       <EventDetailDialog v-if="event !== null" />
+      <EventFormDialog v-if="event !== null" />
     </v-dialog>
   </div>
 </template>
@@ -35,10 +37,12 @@
 import { format } from "date-fns";
 import { mapGetters, mapActions } from "vuex";
 import EventDetailDialog from './EventDetailDialog';
+import EventFormDialog from './EventFormDialog';
 
 export default {
   components: {
     EventDetailDialog,
+    EventFormDialog,
   },
   data() {
     return {
@@ -56,11 +60,18 @@ export default {
     setToday() {
       this.value = format(new Date(), "yyyy/MM/dd");
     },
-    showEvent({ event }) {
+    showEvent({ nativeEvent, event }) {
       this.setEvent(event);
+      nativeEvent.stopPropagation();
     },
     closeDialog() {
       this.setEvent(null);
+    },
+    initEvent({ date }) {
+      date = date.replace(/-/g, '/');
+      const start = format(new Date(date), 'yyyy/MM/dd 00:00:00')
+      const end = format(new Date(date), 'yyyy/MM/dd 01:00:00')
+      this.setEvent({ name: '', start, end, timed: true });
     },
   },
 };
