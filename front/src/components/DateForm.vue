05c7f0841952153556e3f6d41da6c267bc533676
Taka2401 <tkht2401@gmail.com>
2021-11-27 15:40:21 +0900

    add: 予定開始日の選択フォーム追加

---
+++ front/src/components/DateForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <!-- propsで受け取ったvalueは変更不可のため、$emitで変更する -->
+    <v-date-picker
+      :value="value"
+      @input="$emit('input', $event)"
+      no-title
+      locale="ja-ja"
+      :day-format="value => new Date(value).getDate()"
+    ></v-date-picker>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+}
+</script>
