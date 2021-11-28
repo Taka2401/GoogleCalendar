7cb2081a14719792c20d1431df0bd4a346a3119d
Taka2401 <tkht2401@gmail.com>
2021-11-29 00:49:18 +0900

    add: カレンダーの作成機能

--- front/src/store/modules/calendars.js
+++ front/src/store/modules/calendars.js
@@ -5,20 +5,31 @@ const apiUrl = 'http://localhost:3000';
 
 const state = {
   calendars: [],
+  calendar: null,
 };
 
 const getters = {
   calendars: state => state.calendars.map(calendar => serializeCalendar(calendar)),
+  calendar: state => serializeCalendar(state.calendar),
 };
 
 const mutations = {
   setCalendars: (state, calendars) => (state.calendars = calendars),
+  appendCalendar: (state, calendar) => (state.calendars = [...state.calendars, calendar]),
+  setCalendar: (state, calendar) => (state.calendar = calendar),
 };
 
 const actions = {
   async fetchCalendars({ commit }) {
     const res = await axios.get(`${apiUrl}/calendars`);
     commit('setCalendars', res.data);
+  },
+  async createCalendar({ commit }, calendar) {
+    const res = await axios.post(`${apiUrl}/calendars`, calendar);
+    commit('appendCalendar', res.data);
+  },
+  setCalendar({ commit }, calendar) {
+    commit('setCalendar', calendar);
   },
 };
 
