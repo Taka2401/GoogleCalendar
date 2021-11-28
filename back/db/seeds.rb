394e69bf036e379067806fff70e67f4137513d35
Taka2401 <tkht2401@gmail.com>
2021-11-28 19:01:17 +0900

    add: イベントとカレンダーモデルの関連付け

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,6 +1,6 @@
-Event.create!(name: '予定1', start: '2021-11-23 10:00:00', end: '2021-11-23 11:00:00')
-Event.create!(name: '予定2', start: '2021-11-23 10:00:00', end: '2021-11-23 11:00:00')
-Event.create!(name: '予定3', start: '2021-11-23 10:00:00', end: '2021-11-23 11:00:00')
-Event.create!(name: '予定4', start: '2021-11-23 10:00:00', end: '2021-11-23 11:00:00')
+calendar = Calendar.create!(name: '予定')
 
-Calendar.create!(name: '予定')
+Event.create!(name: '予定1', start: '2021-11-28 19:00:00', end: '2021-11-28 20:00:00', calendar: calendar)
+Event.create!(name: '予定2', start: '2021-11-28 19:00:00', end: '2021-11-28 20:00:00', calendar: calendar)
+Event.create!(name: '予定3', start: '2021-11-28 19:00:00', end: '2021-11-28 20:00:00', calendar: calendar)
+Event.create!(name: '予定4', start: '2021-11-28 19:00:00', end: '2021-11-28 20:00:00', calendar: calendar)
