05c5abbd7c4e49ca13f9016f0af0c56fbfa5b024
Taka2401 <tkht2401@gmail.com>
2021-11-28 18:29:11 +0900

    add: calendarモデル作成

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_093712) do
+ActiveRecord::Schema.define(version: 2021_11_28_092658) do
+
+  create_table "calendars", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.string "color"
+    t.boolean "visibility", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 100, null: false
